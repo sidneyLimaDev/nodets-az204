1dee85471c4f8d149485ecfd1833d9f5ea435d98
Sidney Lima <sidneylimadev@gmail.com>
2025-02-02 13:44:29 -0300

    feat: views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("home controller");
-  /* res.render("pages/page"); */
+  /* res.send("home controller"); */
+  res.render("pages/page");
 };
-export function fishes(arg0: string, fishes: any) {
-  throw new Error("Function not implemented.");
-}
 
-export function cats(arg0: string, cats: any) {
-  throw new Error("Function not implemented.");
-}
+export const dogs = (req: Request, res: Response) => {
+  res.send("dogs");
+};
 
-export function dogs(arg0: string, dogs: any) {
-  throw new Error("Function not implemented.");
-}
+export const fishes = (req: Request, res: Response) => {
+  res.send("fishes");
+  /* throw new Error("Function not implemented."); */
+};
+
+export const cats = (req: Request, res: Response) => {
+  res.send("cats");
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import * as SearchController from "../controllers/searchController";
 
 const router = Router();
 
-router.get("/", PageController.home); // Rota inicial
+router.get("/", PageController.home);
 router.get("/dogs", PageController.dogs);
 router.get("/cats", PageController.cats);
 router.get("/fishes", PageController.fishes);
